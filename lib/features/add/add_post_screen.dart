302a0ad684dc2c671e35d4b27e5e17556fb35a9e
Sergio Sánchez <sss4esob@gmail.com>
2023-07-16 13:08:02 +0200

    Implement Add publication screen

--- lib/features/add/add_post_screen.dart
+++ lib/features/add/add_post_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pic_connect/features/add/add_post_bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:camera/camera.dart';
 
 class AddPostScreen extends StatefulWidget {
   const AddPostScreen({Key? key}) : super(key: key);
@@ -9,7 +10,106 @@ class AddPostScreen extends StatefulWidget {
   State<AddPostScreen> createState() => _AddPostScreenState();
 }
 
-class _AddPostScreenState extends State<AddPostScreen> {
+class _AddPostScreenState extends State<AddPostScreen> with WidgetsBindingObserver {
+
+  CameraController? _controller;
+  bool _isCameraInitialized = false;
+  late final List<CameraDescription> _cameras;
+  bool _isRecording = false;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    initCamera();
+  }
+
+  Future<void> initCamera() async {
+    _cameras = await availableCameras();
+    // Initialize the camera with the first camera in the list
+    await onNewCameraSelected(_cameras.first);
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    // App state changed before we got the chance to initialize.
+    final CameraController? cameraController = _controller;
+
+    // App state changed before we got the chance to initialize.
+    if (cameraController == null || !cameraController.value.isInitialized) {
+      return;
+    }
+
+    if (state == AppLifecycleState.inactive) {
+      // Free up memory when camera not active
+      cameraController.dispose();
+    } else if (state == AppLifecycleState.resumed) {
+      // Reinitialize the camera with same properties
+      onNewCameraSelected(cameraController.description);
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  Future<XFile?> capturePhoto() async {
+    final CameraController? cameraController = _controller;
+    if (cameraController!.value.isTakingPicture) {
+      // A capture is already pending, do nothing.
+      return null;
+    }
+    try {
+      await cameraController.setFlashMode(FlashMode.off);
+      XFile file = await cameraController.takePicture();
+      return file;
+    } on CameraException catch (e) {
+      debugPrint('Error occured while taking picture: $e');
+      return null;
+    }
+  }
+
+  Future<XFile?> captureVideo() async {
+    final CameraController? cameraController = _controller;
+    try {
+      setState(() {
+        _isRecording = true;
+      });
+      await cameraController?.startVideoRecording();
+      await Future.delayed(const Duration(seconds: 5));
+      final video = await cameraController?.stopVideoRecording();
+      setState(() {
+        _isRecording = false;
+      });
+      return video;
+    } on CameraException catch (e) {
+      debugPrint('Error occured while taking picture: $e');
+      return null;
+    }
+  }
+
+  void _onTakePhotoPressed() async {
+    final navigator = Navigator.of(context);
+    final xFile = await capturePhoto();
+    if (xFile != null) {
+      if (xFile.path.isNotEmpty) {
+
+      }
+    }
+  }
+
+  void _onRecordVideoPressed() async {
+    final navigator = Navigator.of(context);
+    final xFile = await captureVideo();
+    if (xFile != null) {
+      if (xFile.path.isNotEmpty) {
+
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +118,93 @@ class _AddPostScreenState extends State<AddPostScreen> {
 
         },
         builder: (context, state) {
-          return const Text("Add Post Screen");
+          if (_isCameraInitialized) {
+            return SafeArea(
+              child: Scaffold(
+                body: Column(
+                  children: [
+                    CameraPreview(_controller!),
+                    const SizedBox(
+                      height: 35,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        if (!_isRecording)
+                          ElevatedButton(
+                            onPressed: _onTakePhotoPressed,
+                            style: ElevatedButton.styleFrom(
+                                fixedSize: const Size(70, 70),
+                                shape: const CircleBorder(),
+                                backgroundColor: Colors.white),
+                            child: const Icon(
+                              Icons.camera_alt,
+                              color: Colors.black,
+                              size: 30,
+                            ),
+                          ),
+                        if (!_isRecording) const SizedBox(width: 15),
+                        ElevatedButton(
+                          onPressed:_isRecording? null: _onRecordVideoPressed,
+                          style: ElevatedButton.styleFrom(
+                              fixedSize: const Size(70, 70),
+                              shape: const CircleBorder(),
+                              backgroundColor: Colors.white),
+                          child: Icon(
+                            _isRecording ? Icons.stop : Icons.videocam,
+                            color: Colors.red,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
         });
+  }
+
+  Future<void> onNewCameraSelected(CameraDescription description) async {
+    final previousCameraController = _controller;
+
+    // Instantiating the camera controller
+    final CameraController cameraController = CameraController(
+      description,
+      ResolutionPreset.high,
+      imageFormatGroup: ImageFormatGroup.jpeg,
+    );
+
+    // Initialize controller
+    try {
+      await cameraController.initialize();
+    } on CameraException catch (e) {
+      debugPrint('Error initializing camera: $e');
+    }
+    // Dispose the previous controller
+    await previousCameraController?.dispose();
+
+    // Replace with the new controller
+    if (mounted) {
+      setState(() {
+        _controller = cameraController;
+      });
+    }
+
+    // Update UI if controller updated
+    cameraController.addListener(() {
+      if (mounted) setState(() {});
+    });
+
+    // Update the Boolean
+    if (mounted) {
+      setState(() {
+        _isCameraInitialized = _controller!.value.isInitialized;
+      });
+    }
   }
 }
