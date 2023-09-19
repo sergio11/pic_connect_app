18873131a97dcd487149abd3fa7aa43a0dc8b808
Sergio Sánchez <sss4esob@gmail.com>
2023-09-19 21:42:49 +0200

    Remove autoscale tabbarview package

--- lib/features/profile/profile_screen.dart
+++ lib/features/profile/profile_screen.dart
@@ -1,4 +1,3 @@
-import 'package:autoscale_tabbarview/autoscale_tabbarview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pic_connect/domain/models/post.dart';
